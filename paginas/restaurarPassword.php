1fc186a6469dfeb72cbd8ea6eab85cd65eec169e
yandersantana <ysantanatavares.1996@hotmail.com>
2017-01-24 00:49:32 -0500

    probando ratatatax2 php

---
+++ paginas/restaurarPassword.php
@@ -0,0 +1,132 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="utf-8">
+<title>Mineria PKYK</title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <meta name="description" content="Seguridad Minera">
+<meta name="keywords" content="mineria,drones,seguridad">
+<meta name="author" content="Keila Cuenca Icaza">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+<body>
+
+        <div class="container">
+        <br>
+            <header>
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#ham1">
+                             <span class="sr-only">Menu</span>
+                             <span class="icon-bar"></span>
+                             <span class="icon-bar"></span>
+                             <span class="icon-bar"></span>
+                             <span class="icon-bar"></span>
+                                 
+                            </button>
+                            <a href="../index.html"><img class="img-responsive" src="../imagenes/LOGO5.png" alt="logo" height="150" width=150></a>
+                           
+                        </div>
+                       
+                        <div >
+                        <br>
+                        <a href="login.html"><img class="img-responsive pull-right  hidden-xs" src="../imagenes/3.png" alt="logear" width="100" height="100" ></a>
+                  
+                        </div>
+                        <div class="collapse navbar-collapse" id="ham1">
+                        <ul class="nav navbar-nav">
+                           
+                              <li><a href="about.html">Acerca de</a></li>
+                            <li><a href="nosotros.html">Nosotros</a></li>
+                            <li><a href="servicios.html">Servicios</a></li>
+                              <li><a href="#">Contacto</a></li>
+                            </ul>
+                            
+                            
+
+                        </div>
+                    </div>
+                </nav>
+            
+            </header>
+            <div class="container" id="fondoCeleste" >
+                  <h2>Por favor ingrese su correo electrónico</h2>
+                    <br>
+          <br>
+          <br>
+    
+         <div class="row">
+                <div class="col-sm-3">
+                          <br>
+       <br>
+          <br>
+          <br>
+         <br>   
+                  
+             </div>
+             <div class="col-sm-6 card card-container">
+            <p id="profile-name" class="profile-name-card"></p>
+            <form class="form-signin">
+                <span id="reauth-email" class="reauth-email"></span>
+                <input type="email" id="inputEmail" class="form-control" placeholder="Correo Electrónico" required autofocus>
+            </form>
+
+           
+        </div>
+              <div class="col-sm-3">
+             </div>
+        </div>
+        <div class="row">
+                <div class="col-sm-3">
+             </div>
+                <div class="col-sm-2">
+             </div>
+            <div class="col-sm-2">
+             <button class="btn btn-sm btn-primary btn-block btn-signin" type="submit" >Enviar</button>
+                <br>
+               
+             </div>
+                <div class="col-sm-5">
+                    
+             </div>
+        </div>
+            </div>
+           
+           <div class="visible-xs">
+            
+                          <br>
+               <div id="fondoGris">
+                 
+                   <a href="../paginas/login.html"><img class="img-responsive" src="../imagenes/3.png" width="100" height="100" alt="login" ></a>
+               </div>
+                       <br>
+          <br>
+       <br>
+          <br>
+          <br>
+         <br>        
+            </div>
+            <br>
+      
+                     <div id="derechos" class="hidden-xs">
+                         
+<br>
+                    <h6>Todos los Derechos reservados ©sanmucuva 2017</h6>
+                    <br>
+                </div>
+    </div>
+    
+     <script src="../js/j3.js"></script>
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+     <script src="../js/npm.js"></script>
+    <script src="../js/j1.js"></script>
+     <script src="../js/j2.js"></script>
+    <script src="../js/j4.js"></script>
+    </body>
+ </html>
