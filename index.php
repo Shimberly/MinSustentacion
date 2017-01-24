184a3a36c2580cf6d460359cfecc0908d3156f0e
PaulValle <paul.valle96@gmail.com>
2017-01-24 00:37:51 -0500

    primer round php

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="utf-8">
+<title>Mineria PKYK</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <meta name="description" content="Seguridad Minera">
+<meta name="keywords" content="mineria,drones,seguridad">
+<meta name="author" content="Keila Cuenca Icaza">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+<body>
+
+        <div class="container">
+        <br>
+            <header>
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#ham1">
+                             <span class="sr-only">Menu</span>
+                             <span class="icon-bar"></span>
+                             <span class="icon-bar"></span>
+                             <span class="icon-bar"></span>
+                             <span class="icon-bar"></span>
+                                 
+                            </button>
+                            <a href="index.html"><img class="img-responsive" src="imagenes/LOGO5.png" alt="logo" height="150" width=150></a>
+                           
+                        </div>
+                       
+                        <div >
+                        <br>
+                        <a href="paginas/login.html"><img class="img-responsive pull-right  hidden-xs" src="imagenes/3.png" alt="logear" width="100" height="100" ></a>
+                  
+                        </div>
+                        <div class="collapse navbar-collapse" id="ham1">
+                        <ul class="nav navbar-nav">
+                           
+                              <li><a href="paginas/about.html">Acerca de</a></li>
+                            <li><a href="paginas/nosotros.html">Nosotros</a></li>
+                            <li><a href="paginas/servicios.html">Servicios</a></li>
+                              <li><a href="paginas/contacto.html">Contacto</a></li>
+                            </ul>
+                            
+                            
+
+                        </div>
+                    </div>
+                </nav>
+            
+            </header>
+            <div class="container hidden-xs">
+                
+                    <img class="img-responsive" src="imagenes/redbird-drone-1.jpg"  alt="mineria" width="1150" height="900"> 
+                
+        
+                <div id="derechos">
+                    <br>
+                    <h6>Todos los Derechos reservados ©sanmucuva 2017</h6>
+                    <br>
+                </div>
+            </div>
+           
+           <div class="visible-xs">
+              <img class="img-responsive" src="imagenes/redbird-drone-2.jpg" alt="mineria">
+                          <br>
+               <div id="fondoGris">
+                 
+                   <a href="paginas/login.html"><img class="img-responsive" src="imagenes/3.png" width="100" height="100" alt="login" ></a>
+               </div>
+                           
+            </div>
+          
+        
+    </div>
+    
+    <script src="js/j3.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+     <script src="js/npm.js"></script>
+    <script src="js/j1.js"></script>
+     <script src="js/j2.js"></script>
+    <script src="js/j4.js"></script>
+ 
+    </body>
+ </html>
