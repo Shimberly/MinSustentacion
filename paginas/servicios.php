57624ee4fee5ea983e9d374dc20ba2dc8fd92192
kehylaicaza <keilacicaza@gmail.com>
2017-01-24 00:52:32 -0500

    php x1

---
+++ paginas/servicios.php
@@ -0,0 +1,141 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="utf-8">
+<title>Mineria PKYK</title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <meta name="description" content="Seguridad Minera">
+<meta name="keywords" content="mineria,drones,seguridad">
+<meta name="author" content="Keila Cuenca Icaza">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+<body>
+
+        <div class="container">
+        <br>
+            <header>
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#ham1">
+                             <span class="sr-only">Menu</span>
+                             <span class="icon-bar"></span>
+                             <span class="icon-bar"></span>
+                             <span class="icon-bar"></span>
+                             <span class="icon-bar"></span>
+                                 
+                            </button>
+                            <a href="../index.html"><img class="img-responsive" src="../imagenes/LOGO5.png" alt="logo" height="150" width=150></a>
+                           
+                        </div>
+                       
+                        <div >
+                        <br>
+                        <a href="login.html"><img class="img-responsive pull-right  hidden-xs" src="../imagenes/3.png" alt="logear" width="100" height="100" ></a>
+                  
+                        </div>
+                        <div class="collapse navbar-collapse" id="ham1">
+                        <ul class="nav navbar-nav">
+                           
+                              <li><a href="about.html">Acerca de</a></li>
+                            <li><a href="nosotros.html">Nosotros</a></li>
+                            <li><a href="servicios.html">Servicios</a></li>
+                              <li><a href="contacto.html">Contacto</a></li>
+                            </ul>
+                            
+                            
+
+                        </div>
+                    </div>
+                </nav>
+            
+            </header>
+            <div class="container"  >
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                  <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+
+      </ol>
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner">
+        
+        <div class="item active">
+          <img src="../imagenes/10.jpg" alt="..." class="img-responsive" width="1150" height="900">
+          <div class="carousel-caption">
+            <h1>SERVICIOS</h1>
+          </div>
+        </div>
+          <div class="item">
+          <img src="../imagenes/11.jpg" alt="..." class="img-responsive" width="1150" height="900">
+          
+        </div>
+        <div class="item">
+          <img src="../imagenes/15.jpg" alt="..." class="img-responsive" width="1150" height="900">
+          <div class="carousel-caption">
+             <h1>Servicios</h1>
+          </div>
+        </div>
+            <div class="item">
+          <img src="../imagenes/13.jpg" alt="..." class="img-responsive" width="1150" height="900">
+          <div class="carousel-caption">
+             <h1>Con Drones</h1>
+          </div>
+        </div>
+            <div class="item">
+          <img src="../imagenes/17.jpg" alt="..." class="img-responsive" width="1150" height="900">
+          <div class="carousel-caption">
+             <h1>Servicios</h1>
+          </div>
+        </div>
+      </div>
+
+    </div>
+ 
+
+    </div>
+                
+       <div class="container hidden-xs">
+               
+                
+        
+                <div id="derechos">
+                    <br>
+                    <h6>Todos los Derechos reservados ©sanmucuva 2017</h6>
+                    <br>
+                </div>
+            </div>
+           
+           <div class="visible-xs">
+             
+                          <br>
+               <div id="fondoGris">
+                 
+                   <a href="login.html"><img class="img-responsive" src="../imagenes/3.png" width="100" height="100" alt="login" ></a>
+               </div>
+                           
+            </div>
+     
+           
+           
+    </div>
+    
+    <script src="../js/j3.js"></script>
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+     <script src="../js/npm.js"></script>
+    <script src="../js/j1.js"></script>
+     <script src="../js/j2.js"></script>
+    <script src="../js/j4.js"></script>
+ 
+    </body>
+ </html>
