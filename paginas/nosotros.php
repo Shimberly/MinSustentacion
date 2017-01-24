9dc17c832c3a20767f2b8d3ab8f435907c08af6c
Shimberly <kimmy_11_07@hotmail.com>
2017-01-24 00:57:42 -0500

    anadiendo pagx2

---
+++ paginas/nosotros.php
@@ -0,0 +1,130 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+ <meta charset="utf-8">
+<title>Mineria PKYK</title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <meta name="description" content="Seguridad Minera">
+<meta name="keywords" content="mineria,drones,seguridad">
+<meta name="author" content="Keila Cuenca Icaza">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+<body>
+
+        <div class="container">
+        <br>
+            <header>
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#ham1">
+                             <span class="sr-only">Menu</span>
+                            <span class="icon-bar"></span>
+                             <span class="icon-bar"></span>
+                             <span class="icon-bar"></span>
+                             <span class="icon-bar"></span>
+                                 
+                            </button>
+                            <a href="../index.html"><img class="img-responsive" src="../imagenes/LOGO5.png" alt="logo" height="150" width=150></a>
+                           
+                        </div>
+                        <div >
+                        <br>
+                        <a href="login.html"><img class="img-responsive pull-right  hidden-xs" src="../imagenes/3.png" alt="logear" width="100" height="100" ></a>
+                  
+                        </div>
+                        <div class="collapse navbar-collapse" id="ham1">
+                        <ul class="nav navbar-nav">
+                           
+                              <li><a href="about.html">Acerca de</a></li>
+                            <li><a href="nosotros.html">Nosotros</a></li>
+                            <li><a href="servicios.html">Servicios</a></li>
+                              <li><a href="contacto.html">Contacto</a></li>
+                            </ul>
+                            
+                            
+
+                        </div>
+                    </div>
+                </nav>
+            
+            </header>
+            <div class="main">
+                 <div class='container content_top'>
+                        <div class="row">
+                            <div class="col-md-7 about_left">
+                                
+                               <img src="../imagenes/MISION.jpg" alt=" " class="img-responsive">
+                                 
+                                  <div class="clearfix"></div>
+                               
+                              </div>
+                              <div class="col-md-4">
+                               <h3 class="m_2">Objetivos</h3>
+                                  <br>
+                               <div class="list styled custom-list">
+
+                                    <span class="dropcap">1</span>  <p> Implementar el manejo de herramientas que permitan una mejor producción</p>
+                                   <br><br>
+                                    <span class="dropcap">2</span> <p>Ser una de las empresas líderes en el mercado Nacional e internacional.</p><br><br>
+                                   
+                                    <span class="dropcap">3</span><p>Proporcionar servicios con la ayuda de la tecnologia satisfaciendo los requerimientos de los clientes.</p>
+
+                              </div>
+                            </div>
+                    </div>
+             </div>
+                <br><br>
+            <div class='container content_middle'>
+                <div class="row">
+                    <div class="col-md-12">
+                       
+                        <div class="panel panel-default">
+                              <div class="panel-heading">
+                                <h2 class="panel-title">Misión</h2>
+                              </div>
+                              <div class="panel-body">
+                                    Ser lider en la producción de recursos naturales respetando todas las normas de seguridad prestando todos nuestros servícios a disposición de la ciudadania.
+                              </div>
+                        </div>
+
+
+                        <div class="panel panel-default">
+                              <div class="panel-heading">
+                                <h2 class="panel-title">Visión</h2>
+                              </div>
+                              <div class="panel-body">
+                                    Mantenerse como una de las compañias con mayor productividad del país de tal manera que aumentamos el PIB y disminuimos costos de producción.
+                              </div>
+                        </div>
+                         <h3> Historia</h3>
+                                    <p class="first_desc">Somos una empresa fundada en Noviembre 2016 por Yander Santana, Keyla Cuenca, Paul Valle y Kimberly Muñoz. Somos estudiantes de la Escuela Politécnica Superior del Litoral del Ecuador.</p>
+
+                                   
+                    </div>
+
+                </div>
+             </div>
+
+
+                <div id="derechos">
+                    <h6>Todos los Derechos reservados</h6>
+                </div>
+
+
+        </div>
+    </div>
+        <script src="../js/j3.js"></script>
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+     <script src="../js/npm.js"></script>
+    <script src="../js/j1.js"></script>
+     <script src="../js/j2.js"></script>
+    <script src="../js/j4.js"></script>
+
+        </body>
+     </html>
