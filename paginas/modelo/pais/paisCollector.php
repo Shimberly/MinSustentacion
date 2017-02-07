d1ac64730b6b8ed42123be223d7b70279f73b2c7
kehylaicaza <keilacicaza@gmail.com>
2017-02-06 21:31:44 -0500

    paisCollector

---
+++ paginas/modelo/pais/paisCollector.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once('pais.php');
+include_once("../collector.php");
+
+class paisCollector extends collector
+{
+
+function showEmpresas() {
+    $rows = self::$db->getRows("SELECT * FROM pais");        
+   
+    $arrayPais= array();        
+    foreach ($rows as $c){
+      $aux = new pais($c{'id_pais'},$c{'nombre'});
+      array_push($arrayPais, $aux);
+    }
+    return $arrayPais;        
+  }
+
+    
+
+  }
+?>
+
