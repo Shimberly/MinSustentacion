35e564a5fca512ddfc9e936f83b13cd83e11d8f9
PaulValle <paul.valle96@gmail.com>
2017-02-22 12:44:46 -0500

    ValidarLogin

--- paginas/validarLogin.php
+++ paginas/validarLogin.php
@@ -1,24 +1,37 @@
 <?php
 session_start();
+include_once("var/html/www/ProyectoPSW2016Mineria/paginas/modelo/persona/PersonaCollector.php");
 ?>
 <!DOCTYPE html>
 
 <html>
     <head>
         <title>Login</title>
+    </head>
+    <body>
+        <p>Validando ...</p>
         <?php
 		$email = $_POST['email'];
 		$clave = $_POST['clave'];
-
+        $existe = 0;
+        
+        $PersonaCollectorObj = new PersonaCollector();
+        echo "<p>Buscando</p>";
+        foreach ($PersonaCollectorObj->showPersonas() as $c){
+            if($c->getEmail() == $email && $c->getClave() == $clave){
+                echo "<p>Encontreeee </p>";
+                $existe = 1;
+                $_SESSION['mineria']= $email;
+		          //if (isset($_SESSION['mineria'])){
+                    //echo "<meta HTTP-EQUIV='REFRESH' CONTENT='1;URL=calidad.php'>";
+                    
+                 // }
+            }else{
+                echo "<p>No encontre :c </p>";
+            }
+        }
 		
-		$_SESSION['mineria']= $email;
-		if (isset($_SESSION['mineria'])){
-	      echo "<meta HTTP-EQUIV='REFRESH' CONTENT='1;URL=calidad.php'>";
-   		}
 	   ?>
             
-    </head>
-    <body>
-	
     </body>
 </html>
