7434558d04797173008cf363d77868f9999ea995
Paul Valle <paul.valle96@gmail.com>
2017-02-24 03:32:48 -0500

    administrar BD

--- paginas/administrar.php
+++ paginas/administrar.php
@@ -5,11 +5,10 @@
 <html lang="en">
 <head>
  <meta charset="utf-8">
-<title>Mineria-Calidad</title>
+<title>Administrar BD</title>
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <link href="../css/bootstrap.css" rel="stylesheet">
     <link href="../css/estilo.css">
-
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 		
     <meta name="description" content="Seguridad Minera">
@@ -18,9 +17,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
 <body>
-         <?php
-		                  if (isset($_SESSION['mineria'])){
-            ?>
+
         <div class="container">
         <br>
       <header>
@@ -40,7 +37,9 @@
                     <li class="hidden">
                         <a href="#page-top"></a>
                     </li>
-                 
+                  <?php
+		                  if (isset($_SESSION['mineria'])){
+                        ?>
                <li class=" dropdown"><a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php
                     echo "Hola " .$_SESSION['mineria']
                     ?><span class="caret"></span></a>
@@ -49,7 +48,14 @@
                                 <li><a href="logout.php">Salir</a></li>
                             </ul>
                         </li>
-                       
+                        <?php
+                          }else{    
+                        ?>
+                             <li class="page-scroll"><a  href="paginas/login.php"><img class="img-responsive" src="imagenes/3.png" width="80" height="35" alt="login" ></a>
+                                 </li>
+                             <?php
+                          } 
+                        ?>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
@@ -59,139 +65,9 @@
             
             </header>
             
-          <br>
-<br>  
-         <br>
-<br>  
-          <br>
-<br>  
-         <br>
-<br>
-
-            <div class="container ">
-                <div class="row">
-                    <div class="col-sm-3">
-                            <div style="text-align:center;">
-                            <h1> Mineria UC </h1>
-                            <img alt="logo" class="img-responsive" src="../imagenes/logoCompañiaMinera.png" >
-                                
-                                <ul id="nav-tabs-wrapper" class="nav nav-tabs nav-pills nav-stacked well">
-                                      <li class="active"><a href="#">Calidad de Aire</a></li>
-                                      <li ><a href="produccion.php">Producción</a></li>
-                                      <li><a href="galeria.php">Galería</a></li>
-                                </ul>
-                            
-                            </div>
-                                 
-                    
-                    </div>
-                   
-                    <div class="col-sm-9">
-                        <div class="tab-content">
-                                <div role="tabpanel" class="tab-pane fade in active" id="vtab1">
-                                    <h3>Calidad de Aire</h3>
-                                    <script type="text/javascript">
-						$(function () {
-						    $(document).ready(function () {
-							Highcharts.setOptions({
-							    global: {
-								useUTC: false
-							    }
-							});
-
-							Highcharts.chart('container', {
-							    chart: {
-								type: 'spline',
-								animation: Highcharts.svg, // don't animate in old IE
-								marginRight: 10,
-								events: {
-								    load: function () {
-
-									// set up the updating of the chart each second
-									var series = this.series[0];
-									setInterval(function () {
-									    var x = (new Date()).getTime(), // current time
-										y = Math.random();
-									    series.addPoint([x, y], true, true);
-									}, 1000);
-								    }
-								}
-							    },
-							    title: {
-								text: 'Live random data'
-							    },
-							    xAxis: {
-								type: 'datetime',
-								tickPixelInterval: 150
-							    },
-							    yAxis: {
-								title: {
-								    text: 'Value'
-								},
-								plotLines: [{
-								    value: 0,
-								    width: 1,
-								    color: '#808080'
-								}]
-							    },
-							    tooltip: {
-								formatter: function () {
-								    return '<b>' + this.series.name + '</b><br/>' +
-									Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
-									Highcharts.numberFormat(this.y, 2);
-								}
-							    },
-							    legend: {
-								enabled: false
-							    },
-							    exporting: {
-								enabled: false
-							    },
-							    series: [{
-								name: 'Random data',
-								data: (function () {
-								    // generate an array of random data
-								    var data = [],
-									time = (new Date()).getTime(),
-									i;
-
-								    for (i = -19; i <= 0; i += 1) {
-									data.push({
-									    x: time + i * 1000,
-									    y: Math.random()
-									});
-								    }
-								    return data;
-								}())
-							    }]
-							});
-						    });
-						});
-					</script>
-
-
-					<div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
-						<div class="row">
-						<div class="col-md-6 ">
-							<a href="calidadMensual.php"><div style="background-color:#e0e0eb; text-align:center">
-								<br><br>
-								<p>Mensual</p>
-								<br><br>
-							</div></a>
-	
-						</div>
-						<div class="col-md-6 ">
-							<a href="calidadAnual.php"><div style="background-color:#b3b3b3; text-align:center">
-								<br><br>
-								<p>Anual</p> 
-								<br><br>
-							</div></a>
-	
-						</div>
-                            
-                            <div class="col-md-12 ">
-								<br><br>
-                                <h1>Tablas de la base de datos</h1>
+          <br><br>  
+            <div class="cabecera">
+                                <h1 class="asd">Tablas de la base de datos</h1>
                                 <h2>Paul</h2>
                                 <a href="modelo/produccion/read_produccion.php"#>{ PRODUCCIÓN }</a>
                                 <a href="modelo/detalle_produccion/read_detalle_produccion.php"#>{ DETALLE DE PRODUCCIÓN }</a>
@@ -214,30 +90,23 @@
                                 <a href="modelo/licencia/read_licencia.php"#>{ LICENCIA }</a>
                                 <a href="modelo/dron/read_dron.php"#>{ DRON }</a>
 								<br><br>
+                </div>
 							</div>
                            
-						</div>
-                                     
 	
-							
-						</div>
-                            <div class="row">
-                            <div class="containerCenter">
-                                <a href="administrar.php"><button>Administrar base de datos</button></a>
-                            </div>
-                                </div>
 
-                                </div>
-              
-                </div>
-                </div>
-                    
-            </div>
+
+                                
+
+
+            
+                      <br>
+<br>  
+         <br>
+<br>
             
             
-    
-            
-      <div class="container">
+      <div class="container hidden-xs">
             <div id="margen">
                  
                
@@ -257,8 +126,7 @@
             </div>
             
             
-        
-    </div>
+
 
         
 <script src="../js/j3.js"></script>
@@ -272,10 +140,6 @@
     <script src="https://code.highcharts.com/modules/exporting.js"></script>
     <script src="https://code.highcharts.com/modules/data.js"></script>	
     <script src="https://code.highcharts.com/modules/drilldown.js"></script>
-       <?php
-                          }else{   
-                               echo "<meta HTTP-EQUIV='REFRESH' CONTENT='1;URL=../index.php'>";
-                             } 
-                        ?>
+      
     </body>
  </html>
